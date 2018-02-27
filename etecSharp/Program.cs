13affd65e8d83432a4d0d86b58e4431267d1cb4c
WilkerS1 <walkerxr@hotmail.com>
2018-02-27 10:19:05 -0300

    added a broken program .-.

--- etecSharp/Program.cs
+++ etecSharp/Program.cs
@@ -5,13 +5,27 @@ using System.Text;
 
 namespace etecSharp
 {
-    class Program
-    {
+    class Circle
+    { 
         static void Main(string[] args)
         {
-
             Console.WriteLine("circle area calc.:\n");
+            double radius = 0;
+            
+            if (args == null)
+            {
+                Console.Write("insert radius: ");
+                radius = double.Parse(Console.ReadLine());
+            } else {
+                radius = Convert.ToDouble(args[1]);
+            };
+            
+            Console.WriteLine("radius is " + radius + "\n");
 
+            double cArea = (Math.PI * Math.Pow(radius, 2));
+            Console.WriteLine("area is {0:N3} lol", cArea);
+
+            Console.ReadKey(true);
         }
     }
 }
