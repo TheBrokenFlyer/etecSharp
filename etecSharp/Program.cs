10fefbc548a460d471479d8487ccdd1bc16d808b
WilkerS1 <walkerxr@hotmail.com>
2018-02-27 10:42:47 -0300

    some progress

--- etecSharp/Program.cs
+++ etecSharp/Program.cs
@@ -9,22 +9,45 @@ namespace etecSharp
     { 
         static void Main(string[] args)
         {
-            Console.WriteLine("circle area calc.:\n");
+            
+            fun_A();
+            fun_B();
+
+        }
+
+        static void fun_A()
+        {
+            Console.WriteLine("\n\ncircle area calc.:\n");
             double radius = 0;
+
+           
+            Console.Write("insert radius: ");
+            radius = double.Parse(Console.ReadLine());
             
-            if (args == null)
-            {
-                Console.Write("insert radius: ");
-                radius = double.Parse(Console.ReadLine());
-            } else {
-                radius = Convert.ToDouble(args[1]);
-            };
-            
+
             Console.WriteLine("radius is " + radius + "\n");
 
             double cArea = (Math.PI * Math.Pow(radius, 2));
             Console.WriteLine("area is {0:N3} lol", cArea);
 
+            Console.ReadKey(true);
+        }
+
+        static void fun_B()
+        {
+            Console.WriteLine("\n\nname var stuff.:\n");
+
+            string txA,txB;
+
+            Console.Write("\ninsert top text:       ");
+            txA = Console.ReadLine();
+            Console.Write("\ninsert bottom text:    ");
+            txB = Console.ReadLine();
+
+            Console.WriteLine(
+                "\n1.>\t"   + txA + "\n\t"      + txB +
+                "\n2.>\t\"" + txA + "\" has "   + txA.Length + " characters.\n\t\"" + txB + "\" has " + txB.Length + " characters.");
+            
             Console.ReadKey(true);
         }
     }
