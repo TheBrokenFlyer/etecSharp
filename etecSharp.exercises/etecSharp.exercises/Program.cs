eafa0c8903801a11ee9280ea47255412ebeb6e54
WilkerS1 <walkerxr@hotmail.com>
2018-03-06 21:46:38 -0300

    exercises done

--- etecSharp.exercises/etecSharp.exercises/Program.cs
+++ etecSharp.exercises/etecSharp.exercises/Program.cs
@@ -20,40 +20,41 @@ namespace ConsoleApplication1 {
                             "   5. isAboveEighteen\n" +
                             "   6. convert to US Dollars\n" +
                             "\n" +
-                            "input:>");
+                            "input:> ");
 
-            while (i) {
-                //asks for input
+            while (i) { //asks for input
+                
 
                 key = Console.ReadKey();
+                char input = key.KeyChar;
 
-                switch (key) {
-                    case "1":
+                switch (input) {
+                    case '1':
                         ex1();
                         i = false;
                         break;
-                    case "2":
+                    case '2':
                         ex2();
                         i = false;
                         break;
-                    case "3":
+                    case '3':
                         ex3();
                         i = false;
                         break;
-                    case "4":
+                    case '4':
                         ex4();
                         i = false;
                         break;
-                    case "5":
+                    case '5':
                         ex5();
                         i = false;
                         break;
-                    case "6":
+                    case '6':
                         ex6();
                         i = false;
                         break;
                     default:
-                        Console.Write("invalid.\ninput:>");
+                        Console.Write(" - invalid.\ninput:> ");
                         break;
                 };
             };
@@ -62,12 +63,19 @@ namespace ConsoleApplication1 {
         }
 
         static void ex1() {
-            Console.Write("\n insert name:      ");
+            Console.Write("\n\n\tinsert name:\t");
             string name = Console.ReadLine();
-            Console.Write("\ninsert england:    ");
+            Console.Write("\tinsert england:\t");
             string city = Console.ReadLine();
-            Console.Write("\ninsert age:        ");
+            Console.Write("\tinsert age:\t");
             string age = Console.ReadLine(); //string because i am lazy to fix compiler errors to convert stuff .-.
+
+            Console.WriteLine(
+                "\n\tname:  {0}" +
+                "\n\tcity:  {1}" +
+                "\n\tage:   {2}",
+                name, city, age
+            );
         }
 
         static void ex2() {
@@ -78,41 +86,41 @@ namespace ConsoleApplication1 {
         }
 
         static void ex3() {
-            Console.Write("\ninsert a number:   ");
+            Console.Write("\n\n\tinsert a number:   ");
             int x = int.Parse(Console.ReadLine());
-            Console.WriteLine("\n" + x + " * 2 = " + Math.Pow(x,2));
+            Console.WriteLine("\n\t" + x + " * 2 = " + Math.Pow(x,2));
             Console.ReadKey(true);
         }
 
         static void ex4() {
-            Console.Write("\ninsert A:  ");
+            Console.Write("\n\n\tinsert A:  ");
             double A = int.Parse(Console.ReadLine());
-            Console.Write("\ninsert B:  ");
+            Console.Write("\tinsert B:  ");
             double B = int.Parse(Console.ReadLine());
-            Console.Write("\ninsert C:  ");
+            Console.Write("\tinsert C:  ");
             double C = int.Parse(Console.ReadLine());
 
             double res = Math.Pow(A, 2) + 5 - C / (B - A % 4);
-            Console.Write("\nthe result is {0:N2}", res);
+            Console.Write("\n\tthe result is {0:N2}", res);
         }
 
         static void ex5() {
-            Console.Write("\ninsert an age:     ");
+            Console.Write("\n\n\tinsert an age:     ");
             int x = int.Parse(Console.ReadLine());
             if (x < 18)
             {
-                Console.WriteLine("less than 18.");
+                Console.WriteLine("\tless than 18.");
             }
             else
             {
-                Console.WriteLine("equals or more than 18.");
+                Console.WriteLine("\tequals or more than 18.");
             }
         }
 
         static void ex6() {
-            Console.Write("\ninsert a R$:       ");
-            int x = int.Parse(Console.ReadLine());
-            Console.Write(
+            Console.Write("\n\n\tinsert a R$:       ");
+            float x = float.Parse(Console.ReadLine());
+            Console.Write("\tR$ {0:N2} --> US$ {1:N2}", x, x/3.25);
         }
     }
 }
