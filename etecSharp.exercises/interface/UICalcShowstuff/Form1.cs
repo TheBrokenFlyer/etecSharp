835c6f8abf03c8749cae54965a594a6d22455648
WilkerS1 <walkerxr@hotmail.com>
2018-04-24 22:49:57 -0300

    added UICalcShowstuff (unfinished)

---
+++ etecSharp.exercises/interface/UICalcShowstuff/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1 {
+    public partial class UICalcShowstuff : Form {
+        public UICalcShowstuff() {
+            InitializeComponent();
+        }
+
+
+        private double aa() {
+            //TO BE CONTINUED
+            return System.Convert.ToDouble("1+1");
+        }
+
+        private void textBox1_MouseDown(object sender, MouseEventArgs e) {
+            textBox1.Text = aa() + "";
+        }
+
+        
+    }
+}
