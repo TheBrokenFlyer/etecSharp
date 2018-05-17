630bb449279f1ce6f042c6b6f1a877d2eecd79ed
WilkerS1 <walkerxr@hotmail.com>
2018-05-16 22:36:37 -0300

    changed stuff in 'someKindOfGame'

---
+++ etecSharp.examples/interface/SomeKindOfGame/windowMain.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class windowMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnLose = new System.Windows.Forms.Button();
+            this.btnPoint = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnLose
+            // 
+            this.btnLose.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLose.Location = new System.Drawing.Point(12, 12);
+            this.btnLose.Name = "btnLose";
+            this.btnLose.Size = new System.Drawing.Size(30, 22);
+            this.btnLose.TabIndex = 1;
+            this.btnLose.UseVisualStyleBackColor = true;
+            // 
+            // btnPoint
+            // 
+            this.btnPoint.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnPoint.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnPoint.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPoint.Location = new System.Drawing.Point(12, 12);
+            this.btnPoint.Name = "btnPoint";
+            this.btnPoint.Size = new System.Drawing.Size(30, 22);
+            this.btnPoint.TabIndex = 2;
+            this.btnPoint.TabStop = false;
+            this.btnPoint.UseVisualStyleBackColor = false;
+            this.btnPoint.Click += new System.EventHandler(this.keyAddPoints);
+            this.btnPoint.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyAddPoints);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1200;
+            this.timer1.Tick += new System.EventHandler(this.keyMinusPoints);
+            // 
+            // windowMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(54, 46);
+            this.Controls.Add(this.btnPoint);
+            this.Controls.Add(this.btnLose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "windowMain";
+            this.ShowIcon = false;
+            this.TransparencyKey = System.Drawing.SystemColors.Info;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLose;
+        private System.Windows.Forms.Button btnPoint;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ etecSharp.examples/interface/SomeKindOfGame/windowMain.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1 {
+    public partial class windowMain : Form {
+
+        public windowMain() {
+            InitializeComponent();
+        }
+
+        Size size = new Size(60, 70);
+        int points = 0;
+
+        private void addPoints(object sender) {
+            this.Size = new Size(size.Width+=6, size.Height+=4);
+            this.Text = ++points + "";
+            btnLose.Focus();
+
+            if (timer1.Interval > 500) {
+                timer1.Interval -= 2;
+            }
+
+            Random r = new Random(new Random().Next(9999).GetHashCode());
+            
+            btnPoint.Location = new Point(
+                                        r.Next(12, this.Size.Width-(12*4)),
+                                        r.Next(12, this.Size.Height-(12*4))
+                                    );
+        }
+
+        private void keyAddPoints(object sender, KeyEventArgs e) {
+            addPoints(sender);
+        }
+
+        private void keyAddPoints(object sender, EventArgs e) {
+            addPoints(sender);
+        }
+
+        private void keyMinusPoints(object sender, EventArgs e) {
+            this.Size = new Size(size.Width -= 2, size.Height -= 1);
+            this.Text = --points + "";
+        }
+        
+
+
+    }
+}
