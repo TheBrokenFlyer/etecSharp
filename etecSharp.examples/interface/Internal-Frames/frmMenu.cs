b68504d0201576258f68a1f8c7099fe5e2570bbb
WilkerS1 <walkerxr@hotmail.com>
2018-05-23 05:39:30 -0800

    internalFrames in C#

---
+++ etecSharp.examples/interface/Internal-Frames/frmMenu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1 {
+
+    public partial class frmMenu : Form {
+
+        public frmMenu() {
+            InitializeComponent();
+            //MessageBox.Show("hello", "uwu", MessageBoxButtons.OK);
+            IsMdiContainer = true;
+        }
+
+        private void newWindow(object sender, EventArgs e) {
+            frmSub window = new frmSub();
+            window.MdiParent = this;
+            window.Show();
+        }
+    }
+}
