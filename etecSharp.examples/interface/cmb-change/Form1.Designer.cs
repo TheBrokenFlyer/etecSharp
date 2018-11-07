26b1648d34c56afa9e3b774a05a4d313ae267c6a
Wilker <TheBrokenFlyer@users.noreply.github.com>
2018-11-07 00:40:43 -0200

    a

---
+++ etecSharp.examples/interface/cmb-change/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmb0 = new System.Windows.Forms.ComboBox();
+            this.cmb1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cmb0
+            // 
+            this.cmb0.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb0.FormattingEnabled = true;
+            this.cmb0.Location = new System.Drawing.Point(13, 13);
+            this.cmb0.Name = "cmb0";
+            this.cmb0.Size = new System.Drawing.Size(259, 33);
+            this.cmb0.TabIndex = 0;
+            this.cmb0.SelectedIndexChanged += new System.EventHandler(this.mudar);
+            // 
+            // cmb1
+            // 
+            this.cmb1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb1.FormattingEnabled = true;
+            this.cmb1.Location = new System.Drawing.Point(13, 52);
+            this.cmb1.Name = "cmb1";
+            this.cmb1.Size = new System.Drawing.Size(259, 33);
+            this.cmb1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.cmb1);
+            this.Controls.Add(this.cmb0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmb0;
+        private System.Windows.Forms.ComboBox cmb1;
+    }
+}
+
---
+++ etecSharp.examples/interface/cmb-change/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            cmb0.DataSource = this.itens;
+        }
+
+        string[] itens = {"Pastéis","Cores","Cidades"};
+
+        string[][] tipos = {
+            new string[] {"Queijo","Carne","Pizza"},
+            new string[] {"Amarelo","Verde","Vermelho"},
+            new string[] {"São Paulo","Rio","Dallas","New Home","Snowdin","Possum Springs"}
+        };
+
+        private void mudar(object sender, EventArgs e)
+        {
+            cmb1.DataSource = tipos[cmb0.SelectedIndex];
+        }
+    }
+}
